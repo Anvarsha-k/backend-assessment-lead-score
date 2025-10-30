ff6b65ff97e6b1152c2dcc1c24bfc5ca943fec9a
Anvarsha-k <shaanvar73@gmail.com>
2025-10-30 15:19:54 +0530

    changed routes code to seperate file named app.js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import * as offerController from "./controllers/offerController.js";
+import * as leadController from "./controllers/leadController.js";
+
+//essential initialisation
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//Routes fof the API
+app.post("/offer", offerController.createOffer);
+app.post("/leads/upload", leadController.uploadMiddleware, leadController.uploadLeads);
+app.post("/score", leadController.scoreLeads);
+app.get("/results", leadController.getResults);
+
+export default app;
